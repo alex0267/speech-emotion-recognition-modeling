c0736724dffa984376c7281ea6074143209d2c60
ncouturier <ncouturier@wewyse.com>
2021-09-24 09:31:24 +0200

    added interface to patches

--- project/data_loader/data_loaders.py
+++ project/data_loader/data_loaders.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Callable, Optional, Tuple
+
 
 import numpy as np
 import torch
@@ -7,10 +7,10 @@ import torchaudio
 from torch.utils.data import WeightedRandomSampler
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms
-from torchvision.datasets.folder import ImageFolder as SoundFolder
 
 from base import BaseDataLoader
 from data_loader.transforms import pipelines
+from data_loader.datasets import MySoundFolder
 
 torchaudio.set_audio_backend("sox_io")
 
@@ -41,11 +41,8 @@ class MnistDataLoader(BaseDataLoader):
         )
 
 
-SND_EXTENSIONS = ".wav"
 
 
-def is_sound_file(item: str) -> bool:
-    return Path(item).suffix in SND_EXTENSIONS and (not item.endswith("gitignore"))
 
 
 def collate_fn(batch):
@@ -225,65 +222,3 @@ class DnnDataLoader(BaseDataLoader):
 
         return train_sampler, valid_sampler
 
-
-class MySoundFolder(SoundFolder):
-    """
-    class interfacing with audio files and doing on the fly data augmentation adding a 90 db and substracting 90 db
-    """
-    def __init__(
-        self,
-        root: str,
-        transform: Optional[Callable] = None,
-        target_transform: Optional[Callable] = None,
-        loader: Callable[[str], Any] = None,
-        is_valid_file: Optional[Callable[[str], bool]] = None,
-    ) -> None:
-
-        is_valid_file = is_valid_file or is_sound_file
-
-        super(MySoundFolder, self).__init__(
-            root, transform, target_transform, loader, is_valid_file
-        )
-
-        self.imgs = self.samples
-
-    def __len__(self) -> int:
-        return 3 * len(self.samples)
-
-    def __getitem__(self, index: int) -> Tuple[Any, Any,str, int]:
-        """
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (sample, target, source path, case) where target is class_index of the target class.
-        """
-        from torchaudio.sox_effects import apply_effects_tensor
-
-        _index = index // 3
-        _case = divmod(index, 3)[1]
-        path, target = self.samples[_index]
-        sample = self.loader(path)
-        metadata = torchaudio.info(path)
-        sample_rate = metadata.sample_rate
-        # channels (Optional[int]) – The number of channels
-        # rate (Optional[float]) – Sampling rate
-        # precision (Optional[int]) – Bit depth
-        # length (Opti For sox backend, the number of samples. (frames * channels).
-        # For soundfile backend, the number of frames.
-
-        if _case == 1:
-            effects = [["pitch", "90"]]
-            sample = apply_effects_tensor(
-                sample[0], effects=effects, sample_rate=int(sample_rate)
-            )
-        if _case == 2:
-            effects = [["pitch", "-90"]]
-            sample = apply_effects_tensor(
-                sample[0], effects=effects, sample_rate=int(sample_rate)
-            )
-        if self.transform is not None:
-            sample = self.transform(sample)
-        if self.target_transform is not None:
-            target = self.target_transform(target)
-        return sample, target, path, _case  # sample (tensor, sample rate), target, source path, augmentation case
