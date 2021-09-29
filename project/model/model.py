3ef66d9a82477edffd01e78206fbc6f7f6d8d86d
ncouturier <ncouturier@wewyse.com>
2021-09-29 11:22:45 +0200

    minor change

--- project/model/model.py
+++ project/model/model.py
@@ -45,8 +45,10 @@ class DnnModel(BaseModel):
             in_channels=64, out_channels=64, kernel_size=(3, 3), stride=(1, 1)
         )
         self.conv4_drop = nn.Dropout2d(p=dropout)
-        self.fc1 = nn.Linear(64 * 4, num_classes)
+        #self.fc1 = nn.Linear(64 * 4, num_classes)
         #self.fc1 = nn.Linear(320, num_classes)
+        self.fc1 = nn.Linear(640, num_classes)
+
 
 
     def forward(self, x):
